e9fc3a94700fa06b4d70d153bbb901c1fa99e261
Muhammad Fariz Dzuhreza <mfaridzdzuhreza124@gmail.com>
2022-12-07 16:30:50 +0700

    card list jobs
    menambahkan card pada list jobs

--- src/component/Jobs.js
+++ src/component/Jobs.js
@@ -1,13 +1,62 @@
 import React from "react";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import imgAbout from '../Images/logo_kecil.jpg'
+import { Link } from "react-router-dom";
+
+import "../CSS/Jobs.css"
 
 const Jobs=()=>{
   return(
     <>
-    <Card>
-      <Card.Body>This is some text within a card body.</Card.Body>
-    </Card>
+    <div className="Container_job">
+      <Card className="py-3 container_jobs" style={{ width: '18rem' }}>
+      <Card.Img className="img_about" variant="top" src={imgAbout}/>
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text className="card_text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary" target="blank" href="https://www.jobstreet.co.id/id/job/it-programmer-specialist-4132759?jobId=jobstreet-id-job-4132759&sectionRank=1&token=0~bbeecc5c-a8f7-4d23-acce-2b8337d71492&searchPath=%2Fid%2Fjob-search%2Fweb-jobs%2F&fr=SRP%20View%20In%20New%20Tab">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+      <Card className="py-3 container_jobs" style={{ width: '18rem' }}>
+      <Card.Img className="img_about" variant="top" src={imgAbout}/>
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text className="card_text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+      <Card className="py-3 container_jobs" style={{ width: '18rem' }}>
+      <Card.Img className="img_about" variant="top" src={imgAbout}/>
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text className="card_text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+      <Card className="py-3 container_jobs" style={{ width: '18rem' }}>
+      <Card.Img className="img_about" variant="top" src={imgAbout}/>
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text className="card_text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+      
+      </div>
+      
     </>
   )
 }
